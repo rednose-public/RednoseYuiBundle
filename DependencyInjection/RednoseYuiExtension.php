48362b619f5ca963c1ce709327ae87de7bcc3797
Marc Bontje <marc@rednose.nl>
2014-05-07 20:08:18 +0200

    Updated license headers

--- DependencyInjection/RednoseYuiExtension.php
+++ DependencyInjection/RednoseYuiExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RednoseYuiBundle package.
+ *
+ * (c) RedNose <http://www.rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rednose\YuiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
--- Twig/Extension/ConfigExtension.php
+++ Twig/Extension/ConfigExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RednoseYuiBundle package.
+ *
+ * (c) RedNose <http://www.rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rednose\YuiBundle\Twig\Extension;
 
 use Symfony\Component\Routing\RouterInterface;

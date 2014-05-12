099761d59c59391449737f286b171000e50c181e
Marc Bontje <marc@rednose.nl>
2014-05-12 11:05:19 +0200

    Added Composer scripthandler

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the RednoseYuiBundle package.
+ *
+ * (c) RedNose <http://www.rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rednose\YuiBundle\Composer;
+
+use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Composer\Script\CommandEvent;
+
+class ScriptHandler
+{
+    /**
+     * @param $event CommandEvent A instance
+     */
+    public static function installAssets(CommandEvent $event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+        $webDir = $options['symfony-web-dir'];
+
+        if (!is_dir($webDir)) {
+            echo 'The symfony-web-dir ('.$webDir.') specified in composer.json was not found in '.getcwd().', can not install assets.'.PHP_EOL;
+
+            return;
+        }
+
+        static::executeCommand($event, $appDir, 'rednose:yui:install');
+    }
+
+    protected static function executeCommand(CommandEvent $event, $appDir, $cmd, $timeout = 300)
+    {
+        $php = escapeshellarg(self::getPhp());
+        $console = escapeshellarg($appDir.'/console');
+        if ($event->getIO()->isDecorated()) {
+            $console .= ' --ansi';
+        }
+
+        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) { echo $buffer; });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+        }
+    }
+
+    protected static function getOptions(CommandEvent $event)
+    {
+        $options = array_merge(array(
+            'symfony-app-dir' => 'app',
+            'symfony-web-dir' => 'web',
+        ), $event->getComposer()->getPackage()->getExtra());
+
+        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
+
+        return $options;
+    }
+
+    protected static function getPhp()
+    {
+        $phpFinder = new PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
+    }
+}
